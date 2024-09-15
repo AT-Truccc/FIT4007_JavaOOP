d634b854daceaf77b0056f706f604badedce680e
Dinh Anh Truc <dinhanhtruc2005@gmail.com>
2024-09-15 16:28:31 +0700

    Update Exercise of Week 01

--- W01Ex12/src/Main.java
+++ W01Ex12/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <= 100; i++) {
-            if (i % 7 == 0) {
-                sum += i;
-            }
+        for (int i = 1; i <= (100/7); i++) {
+            sum += i*7;
         }
         System.out.println("Ket qua: "+sum);
     }
