3c2ddecb19955c261db9778a0e9089a00daeeb80
Dinh Anh Truc <dinhanhtruc2005@gmail.com>
2024-09-20 21:14:55 +0700

    Update W01Ex10

--- W01Ex10/src/Main.java
+++ W01Ex10/src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         //Variable
-
-        //Input
         int sum = 0;
 
+        //Input
+        
         //Process
         for (int i = 0; i < 20; i++) {
             if (i % 2 == 0) {
                 sum += i;
             }
         }
-        
+
         //Output
         System.out.println(+sum);
     }
