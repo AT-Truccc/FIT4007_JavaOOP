c7c57bf42c0729ec4fa67384dec012b6cada745c
Dinh Anh Truc <dinhanhtruc2005@gmail.com>
2024-09-20 21:16:22 +0700

    Update

--- W01Ex10/src/Main.java
+++ W01Ex10/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int sum = 0;
 
         //Input
-        
+
         //Process
         for (int i = 0; i < 20; i++) {
             if (i % 2 == 0) {
--- W01Ex12/src/Main.java
+++ W01Ex12/src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //Variable
-        //Input
         int sum = 0;
+
+        //Input
+        
         //Process
         for (int i = 1; i <= (100/7); i++) {
             sum += i*7;
         }
+
         //Output
         System.out.println("Ket qua: "+sum);
     }
