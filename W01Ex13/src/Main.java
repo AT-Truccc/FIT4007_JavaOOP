482a11184ce6418e66d20aa3e6bc6e42c304a4e7
Dinh Anh Truc <dinhanhtruc2005@gmail.com>
2024-09-20 21:38:37 +0700

    W01Ex13

---
+++ W01Ex13/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //Variable
+        int n = 10;
+        int sum = 0;
+
+        //Input
+
+        // Process
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        //Output
+        System.out.println( + sum);
+    }
+}
